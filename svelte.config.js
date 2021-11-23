0cc86e554e942ad87454cd502637e2991438cf82
sumanadak <sumanadak@gmail.com>
2021-11-23 19:51:39 +0530

    changed the adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
